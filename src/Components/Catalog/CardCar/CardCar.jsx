d74780a9d4cb4476fdb4b8ff82696903f2aa4c3b
Yuri <dr.dampel@yandex.ru>
2022-12-20 14:17:08 +0300

    Added list cars. Catalog stiled. Added mockCars.

--- src/Components/Catalog/CardCar/CardCar.jsx
+++ src/Components/Catalog/CardCar/CardCar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import s from './CardCar.module.css';
 
-export const CardCar = () => {
+export const CardCar = (props) => {
     return(
-        <div className={s.outterContainerCard}>
-            <img src="" alt="" />
+        <div className={`${s.outterContainerCard} hoverElement`}>
+            <img src={props.photo} alt=""/>
+            <span className={s.titleAndPrice}>{props.title}<br/>{props.price}</span>
         </div>
     )
 }
--- src/Components/Catalog/Catalog.jsx
+++ src/Components/Catalog/Catalog.jsx
@@ -5,10 +5,34 @@ import businessClass from '../../Assets/CarsClasses/business.png';
 import jeepClass from '../../Assets/CarsClasses/jeep.png';
 import minivenClass from '../../Assets/CarsClasses/miniven.png';
 import sportClass from '../../Assets/CarsClasses/sport.png';
+import { CardCar } from "./CardCar/CardCar";
+import { ScrollArea } from '@mantine/core';
+import businessList from "../../MockData/BussinessCarsMock";
 
 export const Catalog = () => {
 
-    const classesImg = [econonClass, businessClass, sportClass, jeepClass, minivenClass];
+    const classesImg = [
+        {
+            titleType: 'Эконом',
+            typePhoto: econonClass
+        }, 
+        {
+            titleType: 'Бизнес',
+            typePhoto: businessClass
+        }, 
+        {
+            titleType: 'Спорт',
+            typePhoto: sportClass
+        }, 
+        {
+            titleType: 'Внедорожник',
+            typePhoto: jeepClass
+        }, 
+        {
+            titleType: 'Минивен',
+            typePhoto: minivenClass
+        }, 
+    ];
 
     return (
         <div className={s.outterContainerCatalog}>
@@ -19,15 +43,30 @@ export const Catalog = () => {
             <div className={s.carClasses}>
                 {
                     classesImg.map(item =>
-                        <div key={item} className="hoverElement activeElement">
-                            <img src={item} alt="" />
-                        </div>                        
+                        // <div key={item} className="hoverElement activeElement">
+                        //     <img src={item.typePhoto} alt="" />
+                        //     <div>{item.titleType}</div>
+                        // </div>
+                        <div key={item} className={`hoverElement activeElement ${s.typeCarItem}`}>
+                            <img src={item.typePhoto} alt="" />
+                            <div>{item.titleType}</div>
+                        </div>
                     )
                 }
             </div>
-            <div className={s.carsList}>
-
-            </div>
+            <ScrollArea style={{height: '74vh', width: '100%'}}>
+                <div className={s.carsList}>
+                    {/* <CardCar title="Ford Focus 3 от 1500 руб./сут." price="" photo='https://img2.goodfon.ru/original/1280x1024/8/d1/aston-martin-one-77-aston.jpg'/>         
+                    <CardCar text="Ford Focus 3 от 1500 руб./сут." photo='https://img2.goodfon.ru/original/1280x1024/8/d1/aston-martin-one-77-aston.jpg'/>         
+                    <CardCar text="Ford Focus 3 от 1500 руб./сут." photo='https://img2.goodfon.ru/original/1280x1024/8/d1/aston-martin-one-77-aston.jpg'/>         
+                    <CardCar text="Ford Focus 3 от 1500 руб./сут." photo='https://img2.goodfon.ru/original/1280x1024/8/d1/aston-martin-one-77-aston.jpg'/>          */}
+                    {
+                        businessList.map(item => 
+                            <CardCar key={item.title} title={item.title} price={item.price} photo={item.photo}/>
+                        )
+                    }
+                </div>
+            </ScrollArea>
         </div>
     )
 }
