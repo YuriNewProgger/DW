219f2d21178e2ee992c5a2ca32f74df6262d8e1c
Yuri <dr.dampel@yandex.ru>
2022-12-26 12:30:43 +0300

    Change registrations component

--- src/Components/Header/Auth/SignUp/SignUp.jsx
+++ src/Components/Header/Auth/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import s from './SignUp.module.css';
 import { TextInput } from '@mantine/core';
 import { useDispatch } from "react-redux";
@@ -8,47 +8,45 @@ export const SignUp = () => {
 
     const dispatch = useDispatch();
 
-    const setSignInTypeAuth = () => {
-        dispatch(setTypeAuth({typeAuth: 'signin'}));        
-    }
+    useEffect(() => {
+        return () => {
+            dispatch(setTypeAuth({ typeAuth: 'signin' }));
+        }
 
+    }, []);
 
     return (
         <div>
             <TextInput placeholder="Имя" label="Имя" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="Фамилия" label="Фамилия" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="Отчество" label="Отчество" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="Полных лет" label="Возраст" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="+7-000-000-00-00" label="Возраст" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="example@gmail.com" label="Email" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="Логин" label="Логин" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <TextInput placeholder="Пароль" label="Пароль" withAsterisk styles={{
-                    label: { color: '#BBBBBB' }
-                }}/>
+                label: { color: '#BBBBBB' }
+            }} />
             <div className={s.outterContainerBtnSignUp}>
-                {/* <button className="btnCommon">Регистрация</button> */}
                 <div className={s.btnControls}>
-                <button className="btnCommon hoverElement activeElement">
-                    Регистрация
-                </button>
-                <button className="btnCommon hoverElement activeElement" onClick={setSignInTypeAuth}>
-                    Войти
-                </button>
-            </div>
+                    <button className="btnCommon hoverElement activeElement">
+                        Регестрироваться
+                    </button>
+                </div>
             </div>
         </div>
     )
