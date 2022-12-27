f17598bb30ad91ab3971663c57a902bba70f51d7
Yuri <dr.dampel@yandex.ru>
2022-12-27 11:24:19 +0300

    Restyle pop menu. Make exit account.

--- src/Components/Header/PopOverMenu/PopMenu.jsx
+++ src/Components/Header/PopOverMenu/PopMenu.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import s from './PopMenu.module.css';
 import { NavLink } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { getUser, setUser } from "../../../Redux/userSlice";
 
 export const PopMenu = (props) => {
+
+    const dispatch = useDispatch();
+    const user = useSelector(getUser);
+
+
+    const exitAccount = () => {
+        dispatch(setUser(''));
+    }
+
     return (
         <div className={props.isOpen ? `${s.outterContainerPopMenu} ${s.outterContainerPopMenuVisible}` : s.outterContainerPopMenuNoVisible}>
+            <div className={s.outterContainerInfoUser}>
+                <div>{user.surname} {user.name}</div>
+                <div>{user.email}</div>
+                <hr />
+            </div>
             {props.role === 'admin' ?
                 <div className={`${s.linkPA} hoverElement activeElement`}>
-                    <NavLink to="/personalAccount">Админ панель</NavLink>
+                    <NavLink to="/adminPanel">Админ панель</NavLink>
                 </div> : <></>}
             <div className={`${s.linkPA} hoverElement activeElement`}>
                 <NavLink to="/personalAccount">Личный кабинет</NavLink>
             </div>
-            <div className={`${s.linkPA} hoverElement activeElement`}>
-                <NavLink to="/exit">Выйти</NavLink>
-            </div>
+            <div className={`${s.linkPA} hoverElement activeElement`} onClick={exitAccount}>Выйти</div>
         </div>
     )
 }
