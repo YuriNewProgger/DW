12dd3d30bac7f815a718339bd8b6bd8f73817abb
Yuri <dr.dampel@yandex.ru>
2022-12-27 11:42:55 +0300

    Added admin panel. Added categories button to admin panel.

---
+++ src/Components/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './AdminPanel.module.css';
+
+export const AdminPanel = () => {
+    return (
+        <div className={s.outterContainerAdminPanel}>
+            <div className={s.outterContainerCategories}>
+                <button className="btnCommon hoverElement activeElement">Автомобили</button>
+                <button className="btnCommon hoverElement activeElement">Аренда</button>
+                <button className="btnCommon hoverElement activeElement">Пользователи</button>
+                <button className="btnCommon hoverElement activeElement">Чёрный список</button>
+            </div>
+
+        </div>
+    )
+}
--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { AdminPanel } from "../AdminPanel/AdminPanel";
 import { Catalog } from "../Catalog/Catalog";
 import { MainDisplay } from "../MainDisplay/MainDisplay";
 import s from './Body.module.css';
@@ -10,6 +11,7 @@ export const Body = () => {
         <Routes>
             <Route path="/" element={<MainDisplay />} />
             <Route path="/catalog" element={<Catalog />} />
+            <Route path="/adminPanel" element={<AdminPanel/>} />
         </Routes>
     )
 }
